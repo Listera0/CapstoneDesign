48e63791484e3e15fdd882f1f891d02165f0fcfb
YongHo <qkaxhf8823@naver.com>
2023-03-23 09:17:17 +0900

    .

--- Developer Plus/js/modal.js
+++
@@ -1,6 +0,0 @@
-const modal = document.querySelector('.modal');
-const btnOpen = document.querySelector('.signup-btn');
-
-btnOpen.addEventListener('click', () => {
- modal.style.display = 'block';
-});
