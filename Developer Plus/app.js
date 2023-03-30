32d1c0368f8fee377d21e55c092426ee858181ad
YongHo <qkaxhf8823@naver.com>
2023-03-30 12:46:28 +0900

    .

---
+++ Developer Plus/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log('express 서버 실행');
+});
