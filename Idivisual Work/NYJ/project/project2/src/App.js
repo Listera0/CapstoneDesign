339425c4b48b6e8348c8107e7fda65a109ad1d55
spicyfist <103812936+spicyfist@users.noreply.github.com>
2023-05-07 23:37:41 +0900

    .

---
+++ Idivisual Work/NYJ/project/project2/src/App.js
@@ -0,0 +1,250 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="search">
+        <div className="search_text">
+          <input
+            className="input_text"
+            type="text"
+            placeholder="검색내용을 입력해주세요"
+          ></input>
+          <div className="input_button">
+            <button class="input_del">
+              <img
+                src="https://letspl.me/assets/images/ic-del.svg"
+                alt="초기화"
+              ></img>
+            </button>
+            <button class="input_search">
+              <img
+                src="https://letspl.me/assets/images/ic-search.png"
+                alt="검색"
+              ></img>
+            </button>
+          </div>
+        </div>
+        <div className="search_result">
+          <div className="search_num">
+            <h3 className="search_num_text">
+              "백엔드"에 대한 검색 결과 <span>9</span>개
+            </h3>
+          </div>
+          <div className="search_rec_dev">
+            <div className="search_subtitle">
+              개발자 모집 <span>3</span>건
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="thumbnail">
+                    <img src="https://mobiinsidecontent.s3.ap-northeast-2.amazonaws.com/kr/wp-content/uploads/2021/06/09175641/GettyImages-1321766712-scaled.jpg"></img>
+                  </div>
+                  <div className="txtWrap">
+                    <div className="title">[그래픽, 웹 디자이너] 여행역할</div>
+                    <div className="content">
+                      프로젝트에 관해 더 자세히 물어보고 싶으신 분들은 오픈
+                      채팅으로 연락주시면 친절하게
+                      설명해드리겠습니다.https://open.kakao.com/o/sVAjZSaf1.
+                      프로젝트의 시작 동기큰 주제여행을 가려고 하는 사람들끼리
+                      각자의 역할을 정한 뒤 해야 할 일을 처리할 수 있고 단합할
+                      수 있도록 해준다친구들과 여행을 가려고 할 때 계획을 짤
+                      때마다 각자의 역할이 있다는 것을 알게되었고, 그 역할을
+                      이용해서 서비스를 만들면 좋을것같단 생각을 했습니다.
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="thumbnail">
+                    <img src="https://mobiinsidecontent.s3.ap-northeast-2.amazonaws.com/kr/wp-content/uploads/2020/02/10160353/GettyImages-1131776334-scaled.jpg"></img>
+                  </div>
+                  <div className="txtWrap">
+                    <div className="title">
+                      사이드 프로젝트(웹개발) 팀원 모집
+                    </div>
+                    <div className="content">
+                      4~5주 단기간에 프로젝트 완성하실분 구합니다! 기획부터
+                      개발까지 같이 만들어가요! 스터디&amp;네트워킹 목표 및
+                      진행방식 목표 : 4~5주 동안 프로젝트 완성 진행방식 : 온라인
+                      장소/횟수 : 주 3~5회 기간 : 4~5주 참여 조건 필요인원 :
+                      백엔드 개발자 1 명(java, springboot 사용할수 있어야합니다)
+                      프론트엔드 개발자 2명 참여회비 : 같이 정하면
+                      좋을거같습니다 기타 : 열심히 하셔서 프토젝트 완성하실분
+                      구합니다!!!
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="thumbnail">
+                    <img src="https://letspl.s3.ap-northeast-2.amazonaws.com/user/3754/images/%EC%BC%80%EC%9D%B4%ED%98%B8%20%ED%94%84%EB%A1%9C%ED%95%84%2021.png"></img>
+                  </div>
+                  <div className="txtWrap">
+                    <div className="title">
+                      [서울][직장인들을 위한 플랫폼 만들어봐요]
+                    </div>
+                    <div className="content">
+                      안녕하세요 직무전환을 위해 사이드 프로젝트를 진행하려고
+                      합니다!현재는 사이드 프로젝트지만, 후에 앱 런칭과 운영까지
+                      생각하고 있어요프로젝트 인원은 9명이고, 필요에 따라 증원될
+                      수 있습니다.소개｢직장인들을 위한 플랫폼｣－ 함께
+                      만들어가봐요목표： 기깔나는 앱 또는 웹 구현프로젝트에 대해
+                      자세하게 정해진 건 아직 없습니다.다만 직장인들을 대상으로
+                      하는 플랫폼을 만들어보고 싶어요.따라서 다양한 의견을
+                      적극적으로 내주실 분을 적극 환영합니다.같은 의견을 가지신
+                      분들은 지원해주세요!취준생, 직장인 등 성별, 나이, 경력
+                      모두 무관입니다.열정 하나만 가지고 지원해주세요! 포지션 별
+                      인원 모집 상세기획자 : 3명(팀장 포함 기획자 3명)UI UX
+                      디자이너 : 2명프론트 : 2명백엔드 : 2명사용기술백엔드 :
+                      Java, Spring, JPA, C++ 등이 프로젝트를 통해 무언가를 얻을
+                      건지와 구현 기능에 따라서 사용할 기술 같이 정해봐요!프론트
+                      : 미정
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="search_find_dev">
+            <div className="search_subtitle">
+              개발자 찾기 <span>4</span>건
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-3">
+                  <div className="top">
+                    <div className="userPic">
+                      <img src="https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2"></img>
+                    </div>
+                    <div className="right">
+                      <div className="userName">
+                        <p>박용호</p>
+                      </div>
+                      <div className="user_job">
+                        <p>웹프론트엔드</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bottom">
+                    <p>FRONTEND</p>
+                    <p>프론트엔드</p>
+                    <p>웹</p>
+                  </div>
+                </div>
+                <div className="col-3">
+                  <div className="top">
+                    <div className="userPic">
+                      <img src="https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2"></img>
+                    </div>
+                    <div className="right">
+                      <div className="userName">
+                        <p>나윤재</p>
+                      </div>
+                      <div className="user_job">
+                        <p>웹서버</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bottom">
+                    <p>Django</p>
+                    <p>백엔드</p>
+                    <p>SpringBoot</p>
+                  </div>
+                </div>
+                <div className="col-3">
+                  <div className="top">
+                    <div className="userPic">
+                      <img src="https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2"></img>
+                    </div>
+                    <div className="right">
+                      <div className="userName">
+                        <p>임영훈</p>
+                      </div>
+                      <div className="user_job">
+                        <p>웹프론트엔드, 웹 서버</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bottom">
+                    <p>프론트개발</p>
+                    <p>프론트엔드</p>
+                    <p>풀스택웹개발</p>
+                  </div>
+                </div>
+                <div className="col-3">
+                  <div className="top">
+                    <div className="userPic">
+                      <img src="https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2"></img>
+                    </div>
+                    <div className="right">
+                      <div className="userName">
+                        <p>우주은</p>
+                      </div>
+                      <div className="user_job">
+                        <p>웹퍼블리셔</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bottom">
+                    <p>개발자</p>
+                    <p>프론트엔드</p>
+                    <p>백엔드</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="search_story">
+            <div className="search_subtitle">
+              스토리 <span>2</span>건
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-1">
+                  <div className="thumbnail">
+                    <img src="https://yozm.wishket.com/media/news/1663/image008.png"></img>
+                  </div>
+                  <div className="txtWrap">
+                    <div className="title">
+                      프론트엔드 아키텍처의 가장 최근 트렌드는?
+                    </div>
+                    <div className="content">
+                      처음에는 그냥 기능 구현을 하면 되지만 프로젝트의 크기가
+                      커지다 보면 ‘제대로 정리해두지 않으면 정말 안 될 것 같은
+                      순간’들을 맞이하게 됩니다. 그냥 만들면 쉬운 요구사항도
+                      기존 코드에 확장하는 것이 쉽지 않고, 점점 디버깅도
+                      힘들어지는 걸 느낍니다. 이러한 문제에 부딪힌 많은
+                      프론트엔드 개발자가 기존 구조에서 조금씩 더 나은
+                      아키텍처와 프레임워크 형태, 그리고 라이브러리리를
+                      제안했습니다. 덕분에 짧은 시간 안에 새로운 아키텍처들과
+                      방법들이 꾸준히 탄생했습니다.
+                    </div>
+                  </div>
+                </div>
+                <div className="col-1">
+                  <div className="thumbnail">
+                    <img src="https://velog.velcdn.com/images/sehyunny/post/fd9273aa-7a11-4c9b-ba3d-450dd04e6a1c/image.png"></img>
+                  </div>
+                  <div className="txtWrap">
+                    <div className="title">(번역) 프런트엔드 개발의 종말</div>
+                    <div className="content">
+                      지난 몇 달간, 저는 AI에 대해 점점 더 불안해하는 많은
+                      주니어 개발자들과 얘기를 나눴습니다. 그들은 GPT-4와 같은
+                      도구들의 갈수록 발전해가는 인상적인 데모를 보고, AI가
+                      HTML/JS에 능숙해질 무렵에는 자신들이 할 일이 남아있지 않을
+                      것이라고 걱정했습니다.출처 :
+                      https://velog.io/@sehyunny/the-end-of-fe-development
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
